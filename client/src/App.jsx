2359fa7802969e2dc4734933868ffa6144d359b6
Neeraj chauhan <nc1676639@gmail.com>
2026-07-10 08:50:28 +0530

    set up project

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center bg-black text-white justify-center h-screen text-3xl font-bold">
+      hello
+    </div>
+  )
+}
+
+export default App
